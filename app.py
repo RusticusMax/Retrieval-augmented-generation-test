06d53c6e70d730fcb0fca28096ef2a65608bb1d6
RusticusMax <rusticusmax@gmail.com>
2023-09-18 23:15:01 -0400

    dropped DB persist due to issue using multiple embeddings

--- app.py
+++ app.py
@@ -25,7 +25,7 @@ CHROMA_DB_DIR = ".chroma"
 # CHROMA_DB_DIR = CHROMA_BASE_DIR + "/" + os.path.splitext(FILE_NAME)[0]
 
 # os.environ["OPENAI_API_KEY"] = os.environ["THE_KEY"]    # set the API key
-load_dotenv()   # LOad keys from .env file
+load_dotenv()   # Load keys from .env file
 
 #st.set_page_config(page_title=' 🤖Doc Intigotator', layout='wide')
 # st.write("Enter your question below and the AI will answer it.  You can also ask follow up questions.  The AI will remember the context of the conversation.")
@@ -37,20 +37,32 @@ load_dotenv()   # LOad keys from .env file
 
 ## select which embeddings we want to use
 # https://huggingface.co/spaces/mteb/leaderboard
-EMBED_WITH = "OpenAI"  # "OpenAI"  # "HuggingFace"
+EMBED_WITH = "BAAI/bge-base-en"  # Hugging face (local)
+# EMBED_WITH = "text-embedding-ada-002" # Open AI default
 
-if EMBED_WITH == "OpenAI":
-    embeddings = OpenAIEmbeddings()
-    model_name = "'text-embedding-ada-002'"
-    encode_kwargs = {"normalize_embeddings": True}
-elif EMBED_WITH == "HuggingFace":
-    model_name = "BAAI/bge-base-en"
-    encode_kwargs = {"normalize_embeddings": True}
-    embeddings = HuggingFaceBgeEmbeddings(model_name=model_name, model_kwargs={'device': 'cuda'}, encode_kwargs=encode_kwargs)
+encode_kwargs = {"normalize_embeddings": True}
+match EMBED_WITH:
+    case "text-embedding-ada-002":
+        model_name = "'text-embedding-ada-002'"
+        embeddings = OpenAIEmbeddings()
+    case "BAAI/bge-base-en":
+        model_name = "BAAI/bge-base-en"
+        # model_name = "BAAI/bge-large-en-v1.5"
+        embeddings = HuggingFaceBgeEmbeddings(model_name=model_name, model_kwargs={'device': 'cuda'}, encode_kwargs=encode_kwargs)
 
 
-db = Chroma(persist_directory=FILE_DIR + '/' + CHROMA_DB_DIR, embedding_function=embeddings)
-print("Number of existing vector DB elements", db._collection.count())
+# if EMBED_WITH == "OpenAI":
+#     model_name = "'text-embedding-ada-002'"
+#     encode_kwargs = {"normalize_embeddings": True}
+#     embeddings = OpenAIEmbeddings()
+# elif EMBED_WITH == "HuggingFace":
+#     model_name = "BAAI/bge-base-en"
+#     # model_name = "BAAI/bge-large-en-v1.5"
+#     encode_kwargs = {"normalize_embeddings": True}
+#     embeddings = HuggingFaceBgeEmbeddings(model_name=model_name, model_kwargs={'device': 'cuda'}, encode_kwargs=encode_kwargs)
+
+# db = Chroma(persist_directory=FILE_DIR + '/' + CHROMA_DB_DIR, embedding_function=embeddings)
+# print("Number of existing vector DB elements", db._collection.count())
 
 # Is there a better way to check if the DB loaded?
 # Need to find a way to reembedd if using an new embedding model (old DB has old embeddings)
@@ -69,15 +81,14 @@ if True: #  or db._collection.count() < 1: # or not (os.path.exists(FILE_DIR + '
     print("split documents into: ", len(texts))
     # create the vectorestore to use as the index
     # db.from_documents(texts, embeddings, persist_directory=FILE_DIR + '/' + CHROMA_DB_DIR)
-    db = Chroma.from_documents(texts, embeddings, persist_directory=FILE_DIR + '/' + CHROMA_DB_DIR)
-    # print("Number of created vector DB elements", db._collection.count())
+    db = Chroma.from_documents(texts, embeddings) # , persist_directory=FILE_DIR + '/' + CHROMA_DB_DIR)
+    print("Number of created vector DB elements", db._collection.count())
 
 # expose this index in a retriever interface
 retriever = db.as_retriever(search_type="similarity", search_kwargs={"k":2})
 # create a chain to answer questions 
-# useful info
-# https://stackoverflow.com/questions/75774873/openai-chatgpt-gpt-3-5-api-error-this-is-a-chat-model-and-not-supported-in-t
-qa = ConversationalRetrievalChain.from_llm(llm=ChatOpenAI(model='gpt-4-0613'), retriever=retriever)
+# useful info:  https://stackoverflow.com/questions/75774873/openai-chatgpt-gpt-3-5-api-error-this-is-a-chat-model-and-not-supported-in-t
+qa = ConversationalRetrievalChain.from_llm(llm=ChatOpenAI(model='gpt-4-0613'), retriever=retriever, verbose=True)
 chat_history = []
 query = "what is the total number of AI publications?"
 result = qa({"question": query, "chat_history": chat_history})
--- util.py
+++ util.py
@@ -1,5 +1,7 @@
 
-
+"""
+Pretty print a dictionary
+"""
 def print_dict(dict_x, depth=0):
     for key, value in dict_x.items():
         if type(value) is dict:
@@ -21,3 +23,30 @@ def print_dict(dict_x, depth=0):
         else:
             print("   " * depth, key + ":", value)
         print()
+"""
+Make sure a name is suitable for a file name
+"""
+## 
+def valid_fname(fname):
+    return "".join(x for x in fname if x.isalnum() or x in "_- ")
+
+"""
+Dump a sqlite3 database 
+"""
+import sqlite3
+def dump_sqlite3(path = 'example1/.chroma'):
+    conn = sqlite3.connect(path +'/' + 'chroma.sqlite3')
+    c = conn.cursor()
+    # c.execute("SELECT * FROM documents")
+    # c.execute("SELECT name FROM sqlite_schema where type='table' ORDER BY name")
+    c.execute("PRAGMA table_info(embedding_fulltext_search_data)")
+    # c.execute("SELECT * FROM collections")
+    c.execute("SELECT * FROM embedding_fulltext_search_data")
+    rows = c.fetchall()
+    print("count: ", len(rows))
+    for row in rows:
+        print(row[0], len(row[1]))
+    conn.close()
+
+# if __name__ == "__main__":
+#   main()
