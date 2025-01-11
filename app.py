9ff5123c10d93f7c0fd4c725d03b684c7005bf0a
RusticusMax <rusticusmax@gmail.com>
2025-01-10 19:25:37 -0500

    Many things, but begain who's my wife Email test.

--- app.py
+++ app.py
@@ -11,20 +11,34 @@ from langchain.embeddings import OpenAIEmbeddings
 from langchain.embeddings import HuggingFaceBgeEmbeddings
 from langchain.vectorstores import Chroma
 from langchain.chains import ConversationalRetrievalChain
+from langchain.vectorstores.sklearn import ParquetSerializer
 import util
 from dotenv import load_dotenv
 # pip install torch==2.0.1+cu118 -f https://download.pytorch.org/whl/torch_stable.html
 import torch
+from langchain.agents import load_tools
+from langchain.agents import initialize_agent
+
+def normalize_result(result):
+    the_result = result.get("answer", "") + result.get("output", "") # Normailzing for output across Agent and ConversationalRetrievalChain
+    the_question = result.get("question", "") + result.get("input", "") # Normailzing for output across Agent and ConversationalRetrievalChain
+    return the_question, the_result
 
 print("Torch version:",torch.__version__)
 print("Is CUDA enabled?",torch.cuda.is_available())
+
+
+# x = ParquetSerializer("example1/.chroma/english_wikipedia/train-00016-of-00022-d282c8bfe7e88162.parquet")
+# y = x.load()
+# text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
+# texts = text_splitter.split_documents(y)
 
 # FILE_NAME = "example.pdf"
 FILE_DIR = "example1"
 CHROMA_DB_DIR = ".chroma"
 # CHROMA_DB_DIR = CHROMA_BASE_DIR + "/" + os.path.splitext(FILE_NAME)[0]
 
-# os.environ["OPENAI_API_KEY"] = os.environ["THE_KEY"]    # set the API key
+
 load_dotenv()   # Load keys from .env file
 
 #st.set_page_config(page_title=' 🤖Doc Intigotator', layout='wide')
@@ -34,22 +48,48 @@ load_dotenv()   # Load keys from .env file
 # FILE_NAME = st.session_state.doc_name
 
 # openai.api_key = os.environ["OPENAI_API_KEY"]    # set the API key
+## Set Query
+# query = """Salt water pours into a full 20 litre tank at a rate of 2 l/min .
+# The inflow initial concentration is 2 kg/l but after 30 minutes the inflow concentration switches to 1 kg/l .
+# The brine in the tank is well mixed and it drains out at a rate of 2 l/min .
+# Call y the grams of salt in the tank at time t.
+# The tank is initially full of fresh water.
+# Solve the differential equation that models the salt in the tank.
+#  Bonus: What is the maximum kg of salt ever in the tank?"""
+#
+# query ="What time does my mother's flight arrive?"
+#
+query ="What is my mother's name?"
 
 ## select which embeddings we want to use
 # https://huggingface.co/spaces/mteb/leaderboard
-EMBED_WITH = "BAAI/bge-base-en"  # Hugging face (local)
-# EMBED_WITH = "text-embedding-ada-002" # Open AI default
+match 1:
+    case 1:
+        EMBED_WITH = "BAAI/bge-base-en"  # Hugging face (local) 768
+    case 2:
+        EMBED_WITH = "text-embedding-ada-002" # Open AI 1536
+    case 3:
+        EMBED_WITH = "text-embedding-3-small" # Open AI 1536 
+    case 4:
+        EMBED_WITH = "text-embedding-3-large" # Open AI 3072 (but returns 1536?)
+print("Using embeddings: ", EMBED_WITH)
 
 encode_kwargs = {"normalize_embeddings": True}
+
 match EMBED_WITH:
     case "text-embedding-ada-002":
-        model_name = "'text-embedding-ada-002'"
+        model_name = EMBED_WITH
+        embeddings = OpenAIEmbeddings()
+    case "text-embedding-3-small":
+        model_name = EMBED_WITH
+        embeddings = OpenAIEmbeddings()
+    case "text-embedding-3-large":
+        model_name = EMBED_WITH
         embeddings = OpenAIEmbeddings()
     case "BAAI/bge-base-en":
         model_name = "BAAI/bge-base-en"
         # model_name = "BAAI/bge-large-en-v1.5"
         embeddings = HuggingFaceBgeEmbeddings(model_name=model_name, model_kwargs={'device': 'cuda'}, encode_kwargs=encode_kwargs)
-
 
 # if EMBED_WITH == "OpenAI":
 #     model_name = "'text-embedding-ada-002'"
@@ -83,32 +123,49 @@ if True: #  or db._collection.count() < 1: # or not (os.path.exists(FILE_DIR + '
     # db.from_documents(texts, embeddings, persist_directory=FILE_DIR + '/' + CHROMA_DB_DIR)
     db = Chroma.from_documents(texts, embeddings) # , persist_directory=FILE_DIR + '/' + CHROMA_DB_DIR)
     print("Number of created vector DB elements", db._collection.count())
+    #print out the size of the vectors in the database
+    test_vec = embeddings.embed_documents(['test'])
+    print(f"Embedding \"{EMBED_WITH}\" vector size: ", len(test_vec[0]))
 
 # expose this index in a retriever interface
 retriever = db.as_retriever(search_type="similarity", search_kwargs={"k":2})
 # create a chain to answer questions 
 # useful info:  https://stackoverflow.com/questions/75774873/openai-chatgpt-gpt-3-5-api-error-this-is-a-chat-model-and-not-supported-in-t
-qa = ConversationalRetrievalChain.from_llm(llm=ChatOpenAI(model='gpt-4-0613'), retriever=retriever, verbose=True)
-chat_history = []
-query = "what is the total number of AI publications?"
-result = qa({"question": query, "chat_history": chat_history})
+#llm=ChatOpenAI(model='gpt-4-0613')
+llm=ChatOpenAI(model='gpt-4o-mini')
+tool_names = ["wolfram-alpha"]
+tools = load_tools(tool_names)
+agent = initialize_agent(tools, llm, agent="zero-shot-react-description", verbose=True)
 
-# # create a chain to answer questions 
+chat_history = []
+
+USE_TOOLS = False
+
+if USE_TOOLS:
+    agent = initialize_agent(tools, llm, agent="zero-shot-react-description", verbose=True)
+    result = agent({"input": query, "question": query})
+else:
+    qa = ConversationalRetrievalChain.from_llm(llm=llm, retriever=retriever, verbose=True)
+    result = qa({"question": query, "chat_history": chat_history})
+
+
+(the_question, the_response) = normalize_result(result)
+# create a chain to answer questions 
 # qa = RetrievalQA.from_chain_type(
-#     llm=OpenAI(), chain_type="stuff", retriever=retriever, return_source_documents=pipTrue)
+# llm=OpenAI(), chain_type="stuff", retriever=retriever, return_source_documents=pipTrue)
 # query = "what is the total number of AI publications?"
 # result = qa({"query": query})
 # the_list = retriever.get_relevant_documents(query)
 # util.print_dict(result)
-print("Question: ", result["question"])
-print("Answer: ", result["answer"])
+print("Question: ", the_question)
+print("Answer: ", the_response)
 print("")
 
-chat_history = [(query, result["answer"])]
-query = "What is this number divided by 2?"
-result = qa({"question": query, "chat_history": chat_history})
-print("Question: ", result["question"])
-print("Answer: ", result["answer"])
-print("chat_history: ", chat_history)
-print("------------------")
+# chat_history = [(query, the_response)]
+# query = "What is this number divided by 2?"
+# result = qa({"input": query, "question": query,"chat_history": chat_history})
+# print("Question: ", result["question"])
+# print("Answer: ", result["answer"])
+# print("chat_history: ", chat_history)
+# print("------------------")
 # util.print_dict(result)
--- util.py
+++ util.py
@@ -2,13 +2,20 @@
 """
 Pretty print a dictionary
 """
-def print_dict(dict_x, depth=0):
+def print_dict(dict_x, depth = 0, list_limit = 5):
     for key, value in dict_x.items():
         if type(value) is dict:
             print_dict(value, depth + 1)
         elif type(value) is list:
+            list_cnt = list_limit
             for i in value:
-                print("[" + str(i) + "]")
+                if list_cnt > 0:
+                    print("   " * depth, "[" + str(i) + "]")
+                    list_cnt -= 1
+                else:
+                    print("   " * depth, "...")
+                    break
+                # print("[" + str(i) + "]")
         else:
             print("   " * depth, key + ":")
     print("-----------------------------------")
@@ -17,12 +24,19 @@ def print_dict(dict_x, depth=0):
             print_dict(value, depth + 1)
         elif type(value) is list:
             print("   " * depth, key + ":")
+            list_cnt = list_limit
             for i in value:
-                print("   " * depth, "[" + str(i) + "]")
-                print("$$$")
+                if list_cnt > 0:
+                    print("   " * depth, "[" + str(i) + "]")
+                    print("$$$")
+                    list_cnt -= 1
+                else:
+                    print("   " * depth, "...")
+                    break
         else:
             print("   " * depth, key + ":", value)
         print()
+
 """
 Make sure a name is suitable for a file name
 """
@@ -32,6 +46,7 @@ def valid_fname(fname):
 
 """
 Dump a sqlite3 database 
+sqlite3 is the default database for Chroma
 """
 import sqlite3
 def dump_sqlite3(path = 'example1/.chroma'):
@@ -48,5 +63,6 @@ def dump_sqlite3(path = 'example1/.chroma'):
         print(row[0], len(row[1]))
     conn.close()
 
+## Use this to call a until routine for testing
 # if __name__ == "__main__":
 #   main()
